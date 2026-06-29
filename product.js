054f23a3967583821a15099cc1e2298c3f9eea18
manisha332 <manishakp135@gmail.com>
2026-06-29 08:17:19 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+      trim:true
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    }
+    
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
