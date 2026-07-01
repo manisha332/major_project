b9a5c65ff2fcff1778fefbc13a3108e63400fef8
samarpitachaudhuri64-hue <samarpitachaudhuri64@gmail.com>
2026-07-01 11:54:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require(".db");
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+connectDB();
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Mongo Connected"))
-.catch(err => console.log(err));
-
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("Server Running");
+});
+app.use((req, res) => {
+    res.status(404).json({message: "Route not found"});
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
 
